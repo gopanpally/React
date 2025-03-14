b08f63bc9b785d3af8eadbc98d2b22996de7740c
Bheem Reddy Gopanpally <gbheemreddy092@gmail.com>
2025-03-13 20:31:16 -0700

    episode1

--- App.js
+++ App.js
@@ -1,14 +1,7 @@
-const parent = React.createElement("div",{id: "parent"},[
-    React.createElement("div",{id: "child1"}, [
-        React.createElement("h1",{}, "I am h1 tag From Child 1"),
-        React.createElement("h2",{}, "I am h2 tag from Child 1")
-    ]),
-    React.createElement("div",{id: "child2"}, [
-        React.createElement("h1",{}, "I am h1 tag From Child 2"),
-        React.createElement("h2",{}, "I am h2 tag from Child 2")
-    ]),
-]
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-        )
+const jsxHeading = <h1 className="heading">Namaste React Js From JSX</h1>
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
