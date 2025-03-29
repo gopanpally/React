7017b9cf9958e9035a859635c74ff6efbec7d633
Bheem Reddy Gopanpally <gbheemreddy092@gmail.com>
2025-03-29 14:00:37 -0700

    main

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-const heading = React.createElement("h1", {}, "Hello World From React");
+const heading = React.createElement("h1", {id: "heading"}, "Hello World From React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
